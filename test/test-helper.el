b848ced92018afa8cfafe77180cd456f46dbe7ff
Akira Komamura <akira.komamura@gmail.com>
2018-01-04 11:15:19 +0900

    Add test on parsing GitHub URLs

--- test/test-helper.el
+++ test/test-helper.el
@@ -1,3 +1,8 @@
 ;;; test-helper.el --- Helpers for emacs-pg-test.el
 
+(unless (require 'emacs-pg nil 'noerror)
+  (message "attempting to load emacs-pg.el")
+  (load-file "./emacs-pg.el")
+  )
+
 ;;; test-helper.el ends here
