e397480183ba0d76f7496015ebb517348ebb1724
Akira Komamura <akira.komamura@gmail.com>
2018-01-16 15:09:21 +0900

    Add commentary

--- play.el
+++ play.el
@@ -27,7 +27,13 @@
 
 ;;; Commentary:
 
-;; 
+;; Play.el (Play) is a playground for Emacs. Its basic idea is to create
+;; an isolated directory called a sandbox and make it $HOME of Emacs.
+;; Play allows you to easily experiment with various Emacs configuration
+;; repositories available on GitHub, while keeping your current configuration
+;; untouched (almost, except for a stuff for Play). It can also simplify
+;; your workflow in Emacs by hiding irrelevant files and directories
+;; existing in your home directory.
 
 ;;; Code:
 
