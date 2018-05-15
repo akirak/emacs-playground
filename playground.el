fc095956cef9e05540e2bacc38cdc3191442c252
Akira Komamura <akira.komamura@gmail.com>
2018-05-16 03:08:06 +0900

    Update the commentary

--- playground.el
+++ playground.el
@@ -27,13 +27,14 @@
 
 ;;; Commentary:
 
-;; Playground is a playground for Emacs. Its basic idea is to create
-;; an isolated directory called a sandbox and make it $HOME of Emacs.
-;; Playground allows you to easily experiment with various Emacs configuration
-;; repositories available on GitHub, while keeping your current configuration
-;; untouched (almost, except for a stuff for Playground). It can also simplify
-;; your workflow in Emacs by hiding irrelevant files and directories
-;; existing in your home directory.
+;; Playground lets you try out .emacs.d configuration repositories.
+;;
+;; The basic idea behind Playground is to create an isolated directory called
+;; a sandbox and make it $HOME of Emacs. Playground allows you to easily
+;; experiment with various Emacs configuration repositories available on GitHub,
+;; while keeping your current configuration untouched (almost, except for stuffs
+;; related to Playground). It can also simplify your workflow in Emacs by hiding
+;; irrelevant files and directories existing in your home directory.
 
 ;;; Code:
 
